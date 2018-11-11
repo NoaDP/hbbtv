27c967c8d003f91d13bcec35a49cafd2f8e91d4b
NoaDP <36301937+NoaDP@users.noreply.github.com>
2018-11-11 21:46:01 +0100

    Cargada practica

---
+++ js/readJson.js
@@ -0,0 +1,61 @@
+/**
+ * Created by Noa on 10/11/2018.
+ */
+function loadAllJSON()
+{
+    var url = "https://raw.githubusercontent.com/Dualsix/json/master/videoData.json";
+
+    var xmlhttp = new XMLHttpRequest();
+
+    xmlhttp.onreadystatechange=function() {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            didResponse(xmlhttp.responseText);
+        }
+    };
+
+    xmlhttp.overrideMimeType("application/json");
+    xmlhttp.open("GET", url, true);
+
+    xmlhttp.send(null);
+
+}
+
+function didResponse(response){
+    var jsonArray = JSON.parse(response);
+    var videos = JSON.stringify(jsonArray);
+    localStorage.setItem('videoListStorage', videos);
+
+}
+
+function images() {
+    var videos = JSON.parse(localStorage.getItem("videoListStorage"));
+    for (i = 1; i <= 7; i++) {
+        document.getElementById("imagen" + i).src = videos.datos[i-1].ImgUrl;
+        document.getElementById("imagen" + i).style.height = "90px";
+        document.getElementById("imagen" + i).style.width = "90px";
+    }
+}
+
+function loadAutor() {
+    var videos = JSON.parse(localStorage.getItem("videoListStorage"));
+    console.log(videos);
+    for (i = 1; i <= 7; i++) {
+        document.getElementById("artist" + i).innerHTML = videos.datos[i-1].Author;
+    }
+}
+
+function loadtitulo() {
+    var videos = JSON.parse(localStorage.getItem("videoListStorage"));
+    console.log(videos);
+    for (i = 1; i <= 7; i++) {
+        document.getElementById("album" + i).innerHTML = videos.datos[i-1].Titulo;
+    }
+}
+
+function loadVisitas() {
+    var videos = JSON.parse(localStorage.getItem("videoListStorage"));
+    console.log(videos);
+    for (i = 1; i <= 7; i++) {
+        document.getElementById("view" + i).innerHTML = videos.datos[i - 1].Visitas;
+    }
+}
